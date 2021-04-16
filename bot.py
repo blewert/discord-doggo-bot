a71fd20d18e8bc796261cb301dbe5f401722949b
ben <trewelb@gmail.com>
2021-04-16 02:02:26 +0100

    Added basic commands for bot

--- bot.py
+++ bot.py
@@ -6,6 +6,8 @@
 
 import os
 import discord
+from discord.ext import commands
+
 from flickr import FlickrPhotosAPI
 
 def get_token_from_file(filename):
@@ -21,6 +23,7 @@ def get_token_from_file(filename):
 
 # Get tokens
 bot_token = get_token_from_file(".token");
+owner_id = get_token_from_file(".owner_id");
 flickr_token = get_token_from_file(".flickr_api_key");
 
 # Get flickr api search instance
@@ -31,12 +34,35 @@ flickr_api.random_search(["corgi", "dog"]);
 
 
 
-# # Make new client
-# bot_client = discord.Client();
+# Make new client
+bot = commands.Bot(command_prefix='!');
 
-# @bot_client.event
-# async def on_ready():
-#     print(f'{bot_client.user} connected! Token is <{bot_token}>');
+@bot.event
+async def on_ready():
+    print(f'{bot.user} connected! Token is <{bot_token}>');
 
-# # Run ittttttt
-# bot_client.run(bot_token);
+
+@bot.command()
+async def puppy(ctx):
+
+    # They're a bot for some reason? Die
+    if ctx.author.bot:
+        return;
+
+    # Otherwise! Do this
+    return await ctx.send("hello");
+
+@bot.command()
+async def quit(ctx):
+
+    # Not the owner? Get out of town
+    if str(ctx.author.id) != owner_id:
+        return await ctx.send("Nice try");
+
+    # Otherwise:
+    print("Quit command found! Closing!");
+    await ctx.send("Leaving!");
+    await ctx.bot.close();
+
+# Run ittttttt
+bot.run(bot_token);
